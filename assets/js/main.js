00cc766fbf04ad98ced3eaa39dad63001e83871a
Matthew Da Silva <matthewgdasilva25@gmail.com>
2024-08-12 15:26:31 +0100

    add keybinds for remaining tracks

--- assets/js/main.js
+++ assets/js/main.js
@@ -57,11 +57,62 @@ const tracks = [
     { name: 'Cymbal', sound: cymbal, trackDivName: "cymbal-track" }
 ];
 
-const keyMap = {
+const kickKeyMap = {
     '1': 0, '2': 1, '3': 2, '4': 3, 
     '5': 4, '6': 5, '7': 6, '8': 7, 
     '!': 8, '@': 9, '#': 10, '$': 11, 
     '%': 12, '^': 13, '&': 14, '*': 15
+};
+
+const kickReverseKeyMap = {
+    0: '1', 1: '2', 2: '3', 3: '4', 
+    4: '5', 5: '6', 6: '7', 7: '8', 
+    8: '!', 9: '@', 10: '£', 11: '%', 
+    12: '^', 13: '&', 14: '*', 15: '('
+};
+
+
+const snareKeyMap = {
+    'q': 0, 'w': 1, 'e': 2, 'r': 3, 
+    't': 4, 'y': 5, 'u': 6, 'i': 7, 
+    'Q': 8, 'W': 9, 'E': 10, 'R': 11, 
+    'T': 12, 'Y': 13, 'U': 14, 'I': 15
+};
+
+const snareReverseKeyMap = {
+    0: 'q', 1: 'w', 2: 'e', 3: 'r', 
+    4: 't', 5: 'y', 6: 'u', 7: 'i', 
+    8: 'Q', 9: 'W', 10: 'E', 11: 'R', 
+    12: 'T', 13: 'Y', 14: 'U', 15: 'I'
+};
+
+
+const hatKeyMap = {
+    'a': 0, 's': 1, 'd': 2, 'f': 3, 
+    'g': 4, 'h': 5, 'j': 6, 'k': 7, 
+    'A': 8, 'S': 9, 'D': 10, 'F': 11, 
+    'G': 12, 'H': 13, 'J': 14, 'K': 15
+};
+
+const hatReverseKeyMap = {
+    0: 'a', 1: 's', 2: 'd', 3: 'f', 
+    4: 'g', 5: 'h', 6: 'j', 7: 'k', 
+    8: 'A', 9: 'S', 10: 'D', 11: 'F', 
+    12: 'G', 13: 'H', 14: 'J', 15: 'K'
+};
+
+const cymbalKeyMap = {
+    'z': 0, 'x': 1, 'c': 2, 'v': 3, 
+    'b': 4, 'n': 5, 'm': 6, ',': 7, 
+    'Z': 8, 'X': 9, 'C': 10, 'V': 11, 
+    'B': 12, 'N': 13, 'M': 14, '<': 15
+};
+
+const cymbalReverseKeyMap = {
+    0: 'z', 1: 'x', 2: 'c', 3: 'v', 
+    4: 'b', 5: 'n', 6: 'm', 7: ',', 
+    8: 'Z', 9: 'X', 10: 'C', 11: 'V', 
+    12: 'B', 13: 'N', 14: 'M', 15: '<'
 };
 
 function createTrack(name, soundName, trackDivName){
@@ -75,6 +126,7 @@ function createTrack(name, soundName, trackDivName){
      for (let i = 0; i < 16; i++) {
          const step = document.createElement('div');
          step.className = 'step';
+         if(i>7){step.classList.add('alt')}
          step.dataset.sound = soundName;
          step.dataset.step = i;
          step.addEventListener('click', function() {
@@ -83,6 +135,22 @@ function createTrack(name, soundName, trackDivName){
             //  this.classList.toggle('active');
             toggleStep(soundName, stepIndex);
          });
+         stepsDiv.appendChild(step);
+
+         const keyLabel = document.createElement('span');
+         keyLabel.className = 'key-label';
+         if(soundName == "kick"){
+            keyLabel.innerText = kickReverseKeyMap[i];
+         } else if(soundName == "snare"){
+            keyLabel.innerText = snareReverseKeyMap[i];
+         }else if(soundName == "hat"){
+            keyLabel.innerText = hatReverseKeyMap[i];
+         }else if(soundName == "cymbal"){
+            keyLabel.innerText = cymbalReverseKeyMap[i];
+         }
+         
+         step.appendChild(keyLabel);
+ 
          stepsDiv.appendChild(step);
      }
  
@@ -102,13 +170,6 @@ startBtn.addEventListener('click', async () => {
     startBtn.style.display = "none";
     tracks.forEach(track => createTrack(track.name, track.name.toLowerCase(), track.trackDivName));
 });
-
-// // Handle step click
-// document.querySelectorAll('.step').forEach(step => {
-//     step.addEventListener('click', function() {
-//         toggleStep(soundName, step.dataset.step);
-//     });
-// });
 
 // Function to toggle a step
 function toggleStep(trackName, stepIndex) {
@@ -171,8 +232,23 @@ document.getElementById('distortionSlider').addEventListener('input', (event) =>
 
 // Keyboard listener
 document.addEventListener('keydown', (event) => {
-    const stepIndex = keyMap[event.key];
+    const stepIndex = kickKeyMap[event.key];
     if (stepIndex !== undefined) {
         toggleStep('kick', stepIndex);
+    }
+
+    const snareStepIndex = snareKeyMap[event.key];
+    if (snareStepIndex !== undefined) {
+        toggleStep('snare', snareStepIndex);
+    }
+
+    const hatStepIndex = hatKeyMap[event.key];
+    if (hatStepIndex !== undefined) {
+        toggleStep('hat', hatStepIndex);
+    }
+
+    const cymbalStepIndex = cymbalKeyMap[event.key];
+    if (cymbalStepIndex !== undefined) {
+        toggleStep('cymbal', cymbalStepIndex);
     }
 });
